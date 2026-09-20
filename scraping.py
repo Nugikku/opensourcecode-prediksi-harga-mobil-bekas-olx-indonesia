51e8375de56afc542a36f45185a6d452c1679558
Nugikku <nugrohosaputrajt@gmail.com>
2026-09-20 22:37:13 +0700

    fix script scarping kurang tanda koma pada link web

--- scraping.py
+++ scraping.py
@@ -66,7 +66,7 @@ async def scrape_olx(target_data=3000):
             "https://www.olx.co.id/mobil-bekas_c198/honda_m27",
             "https://www.olx.co.id/mobil-bekas_c198/daihatsu_m19",
             "https://www.olx.co.id/mobil-bekas_c198/suzuki_m52",
-            "https://www.olx.co.id/mobil-bekas_c198/mitsubishi_m42"
+            "https://www.olx.co.id/mobil-bekas_c198/mitsubishi_m42",
             # Kategori berdasarkan Wilayah
             "https://www.olx.co.id/jakarta-dki_g2000007/mobil-bekas_c198",
             "https://www.olx.co.id/jawa-barat_g2000008/mobil-bekas_c198",
